caa79713249bc1c2741a7cc82c22af58ad1115ca
Roy Sindre Norangshol <norangshol@zedge.net>
2019-04-16 15:13:53 +0200

    Support not scraping workers metrics
    For our use case we run RQ in GCP (Google Cloud Platform) with pods
    running on preemtible nodes in Kubernetes, which means new workes
    arrives and goes at will. Since each new spawned pod will have it's own
    new unique hostname, we will be flooding our prometheus server with
    useless metrics.
    
    This feature allows us to avoid flooing our prometheus server with
    useless metrics.
    
    Default behavior is to scrape this metrics, just as before.
    
    You can disable this metric with setting the environment varaible
    `RQPE_SCRAPE_WORKERS` to anything else then 'true', 'yes', 't' or '1'.

--- stats_test.py
+++ stats_test.py
@@ -1,5 +1,5 @@
 import stats
-from rq import Worker, Queue, Connection
+from rq import Worker, Queue
 
 queue = Queue("echo", connection=stats.CONN)
 worker = Worker(["searches", "test"], name="test_worker", connection=stats.CONN)
@@ -8,11 +8,29 @@ def teardown_function():
     queue.delete(delete_jobs=True)
     worker.register_death()
 
-def test_stat_scrape():
+def test_stat_scrape_default():
     queue.enqueue("echo_worker", "HEY!")
     queue.enqueue("echo_worker", "HO!")
     worker.register_birth()
 
+    jobs, workers = stats.scrape()
+    assert jobs == [{"queue_name": "echo", "size": 2}]
+    assert workers == [{"name": "test_worker", "queues": "searches,test", "state": "?"}]
+
+def test_stat_scrape_disabled_workers_scraping():
+    queue.enqueue("echo_worker", "HEY!")
+    queue.enqueue("echo_worker", "HO!")
+    worker.register_birth()
+    stats.SCRAPE_WORKERS = False
+    jobs, workers = stats.scrape()
+    assert jobs == [{"queue_name": "echo", "size": 2}]
+    assert workers == []
+
+def test_stat_scrape_enabled_workers_scraping():
+    queue.enqueue("echo_worker", "HEY!")
+    queue.enqueue("echo_worker", "HO!")
+    worker.register_birth()
+    stats.SCRAPE_WORKERS = True
     jobs, workers = stats.scrape()
     assert jobs == [{"queue_name": "echo", "size": 2}]
     assert workers == [{"name": "test_worker", "queues": "searches,test", "state": "?"}]
